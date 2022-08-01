806898526419e00ad3a1b5739a6dc270498d2e65
aaronw9989 <williamsa493@strose.edu>
2022-08-01 16:03:08 -0500

    Completed lab 8.1, implementing lambdas.

--- Lab08.1/src/com/javatunes/compare/StringLengthComparator.java
+++ Lab08.1/src/com/javatunes/compare/StringLengthComparator.java
@@ -27,6 +27,15 @@ implements Comparator<String> {
    */
   @Override
   public int compare(String str1, String str2) {
-    return str1.length() - str2.length();
+
+
+    if(str1.length() < str2.length()) {
+      return -1;
+    } else if (str1.length() > str2.length()) {
+      return 1;
+    } else {
+      return 0;
+    }
+
   }
 }
--- Lab08.1/test/com/javatunes/compare/StringSortTest.java
+++ Lab08.1/test/com/javatunes/compare/StringSortTest.java
@@ -9,29 +9,51 @@
 package com.javatunes.compare;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.Test;
 
 public class StringSortTest {
-  
-  private List<String> names = Arrays.asList("Jason", "Martin", "Jack", "Mary", "Jill", "Frederick", "Laurie", "Ed");
-  
+
+  private List<String> names = Arrays.asList("Jason", "Martin", "Jack", "Mary", "Jill", "Frederick",
+      "Laurie", "Ed");
+
   @Test
   public void testStringListSort() {
     System.out.println("Order will be added to following list:");
     System.out.println(names + "\n");
-    
+
     System.out.println("Natural order:");
     names.sort(null);
     System.out.println(names + "\n");
-    
+
     System.out.println("Increasing length - Comparator class:");
     names.sort(new StringLengthComparator());
     System.out.println(names + "\n");
-    
+
     System.out.println("Increasing length - lambda:");
     // TODO: call names.sort() and pass in an "expression" lambda
     // hint: a compatible lambda will have this form:
     //       (obj1, obj2) -> expression-that-evaluates-to-int
+    // names.sort((a, b) -> a.length() - b.length());
+
+
+    names.sort((a, b) -> a.length() - b.length());
+
+    System.out.println(names);
+
+    names.sort((a, b) -> {
+      if (a.length() < b.length()) {
+        return -1;
+      } else if (a.length() > b.length()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    // Method reference lambda
+    names.sort(Comparator.comparing(String::length));
+
   }
 }
